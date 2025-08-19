bc516744f5adb636d5bf71c1ef0e42a4ea5fb25e
fdjasfjlksdjfaiosjfne <nguyentiendung1@proton.me>
2025-08-19 15:28:43 +0700

    Start of remaking the config processing system.

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -14,12 +14,15 @@ dotenv_vault.load_dotenv()
 sys.path.insert(0, os.getenv("ROOT_PATH") or "")
 
 from backend import errors
-from backend.config.dataclass import (
-    ConfigVersionCls,
+from backend.config.dataclass import ConfigVersionCls, RootConfigCls
+from backend.config.checks import asserting_config_dict
 
-)
+FIELD_ALIASES = {
+    "customization": ["customization", "customisation"],
+    "customisation": ["customization", "customisation"],
+}
 
-LASTEST_VERSION = ConfigVersionCls(2, 0, 0, "indev")
+LASTEST_VERSION = ConfigVersionCls(major = 2, minor = 1, patch = 1, phase = "indev")
 CAMEL_TO_SNAKE_PATTERN = regex.compile('((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
 
 def camel_to_snake(name: str) -> str:
@@ -34,104 +37,99 @@ def switch_casing(conf: dict) -> dict:
             d[camel_to_snake(k)] = v
     return d
 
-def solidify_config(conf: dict[str, typing.Any]) -> ConfigCls:
+def modify_template_option(opt):
+    match opt:
+        case 0 | "disabled" | False:
+            return "disabled"
+        case 1 | "enabled" | True:
+            return "enabled"
+        case 2 | "forced":
+            return "forced"
+        case _:
+            raise errors.InternalError(f"modify_template_option recieve an invalid value: {opt}")
+
+def _solidify_config_dict[T1, T2: dict](cls: type[T1], data: T2) -> T1 | T2:
+    """
+    Recursively build dataclass instances from nested dictionaries.
+    Automatically handles nested dataclasses and missing keys.
+    """
+    assert dataclasses.is_dataclass(cls)
+    if not hasattr(cls, "__dataclass_fields__"):
+        return data
+    
+    kwargs = {}
+    type_hints = typing.get_type_hints(cls)
+
+    for field in dataclasses.fields(cls):
+        field_name = field.name
+        field_type = type_hints.get(field_name, field.type)
+        field_value = None
+        found_keys = []
+
+        possible_names = FIELD_ALIASES.get(field_name, [field_name])
+        for possible_name in possible_names:
+            if possible_name in data:
+                found_keys.append(possible_name)
+                field_value = data[possible_name]
+        
+        if len(found_keys) > 1:
+            raise errors.ConfigError(
+                f"Multiple spellings found for field '{field_name}': {found_keys}. "
+                f"Use only one of: {possible_names}"
+            )
+        
+        if field_value is not None:
+            if hasattr(field_type, "__dataclass_fields__"):
+                kwargs[field_name] = _solidify_config_dict(field_type, field_value)
+            else:
+                kwargs[field_name] = field_value
+    
+    return cls(**kwargs)
+
+def solidify_config(conf: dict[str, typing.Any]) -> RootConfigCls:
     conf_ = switch_casing(conf)
-    if "customization" in conf_ and "customisation" in conf_:
-        raise errors.ConfigError(
-            "Both the American and British spelling of a configuration option"
-            f"('customization' and 'customisation' respectively) is present"
-        )
-    customs: dict[str, typing.Any] = conf_.pop("customization", conf_.pop("customisation", {}))
-    redef: dict = customs.pop("redefine", {})
-    int_bases: dict = customs.pop("integer_base_literals", {})
-    config_version: dict = conf_.pop("config_version", LASTEST_VERSION)
-    cus_opts: dict = conf_.get("custom_options", {})
-    templates: dict = conf_.get("template", {})
-    mtc: str = redef.pop("multi_line_comment", "/* */")
-    mtlcs, mtlce = mtc.split()
+
+    try:
+        asserting_config_dict(conf_)
+    except AssertionError as e:
+        raise errors.ConfigError(*e.args)
     
-    # & I'm not adding multi-version support
-    # & What's so important about supporting older development versions anyway?
-    # & They just exist for like, a few days at most
-    # & I'll add them once it's mainstream...Which is basically never.
-    if config_version != dataclasses.asdict(LASTEST_VERSION):
-        raise errors.InternalError(
-            "Due to the developer having an overdose of laziness, version "
-            f"{".".join(str(i) for i in config_version)} is not supported. "
-            f"Please use version {".".join(str(i) for i in dataclasses.astuple(LASTEST_VERSION))} "
-            "instead."
-        )
-
-    # ^ languageCustomizationModes
-    customization_mode_replace_dict = {
-        "disabled": CustomizationMode.Disabled,
-        "enabled": CustomizationMode.Enabled,
-        "forced": CustomizationMode.Forced,
-
-        0: CustomizationMode.Disabled,
-        1: CustomizationMode.Enabled,
-        2: CustomizationMode.Forced,
-
-        False: CustomizationMode.Disabled,
-        True: CustomizationMode.Enabled,
-    }
-
-    templates = {
-        k: customization_mode_replace_dict.get(v, CustomizationMode.Disabled) for k, v
-        in templates.items()
-    }
-
-    for american, british in [("mutable_value_assignment_behavior", "mutable_value_assignment_behaviour"), 
-                              ("mutable_argument_default_value_behavior", "mutable_argument_default_value_behaviour"), 
-                              ("logical_operator_behavior", "logical_operator_behaviour")]:
-        if british in customs:
-            if american in customs:
-                raise errors.ConfigError(
-                    "Both the American and British spelling of a configuration option"
-                    f"('{american}' and '{british}' respectively) is present"
-                )
-            customs[american] = customs[british]
-            del customs[british]
-
-    # ^ Check for identical values
-    redefs: dict[str, list] = {}
-    for k, val in redef.items():
-        if val is None:
-            continue
-        if val not in redefs:
-            redefs[val] = []
-        redefs[val].append(k)
+    return typing.cast(RootConfigCls, _solidify_config_dict(RootConfigCls, conf_))
     
-    duplicates = [v for v in redefs.values() if len(v) == 2]
-    if duplicates:
-        if len(duplicates) > 1:
-            start_msg = "Duplicates have"
-        else:
-            start_msg = "Duplicate has"
-        raise errors.ConfigError(
-            f"{start_msg} been found: {"; ".join(str(i) for i in duplicates)}"
-        )
-    del duplicates
-
-    if int_bases.values() and not any(int_bases.values()):
-        raise errors.ConfigError(
-            "At least 1 of the integer bases should be allowed"
-        )
+    # # ^ Check for identical values
+    # redefs: dict[str, list] = {}
+    # for k, val in redef.items():
+    #     if val is None:
+    #         continue
+    #     if val not in redefs:
+    #         redefs[val] = []
+    #     redefs[val].append(k)
     
-    return ConfigCls(
-        CustomizationCls(
-            redefine = RedefineCls(
-                multi_line_comment = MultiLineComment(mtlcs, mtlce),
-                **redef
-            ),
-            integer_base_literals = IntegerBaseLiterals(**int_bases),
-            **customs
-        ),
-        TemplateCls(
-            **templates
-        ),
-        **cus_opts
-    )
+    # duplicates = [v for v in redefs.values() if len(v) == 2]
+    # if duplicates:
+    #     if len(duplicates) > 1:
+    #         start_msg = "Duplicates have"
+    #     else:
+    #         start_msg = "Duplicate has"
+    #     raise errors.ConfigError(
+    #         f"{start_msg} been found: {"; ".join(str(i) for i in duplicates)}"
+    #     )
+    # del duplicates
+    
+    # return ConfigCls(
+    #     CustomizationCls(
+    #         redefine = RedefineCls(
+    #             multi_line_comment = MultiLineComment(mtlcs, mtlce),
+    #             **redef
+    #         ),
+    #         integer_base_literals = IntegerBaseLiterals(**int_bases),
+    #         **customs
+    #     ),
+    #     TemplateCls(
+    #         **templates
+    #     ),
+    #     **cus_opts
+    # )
 
 #CONFIG = solidify_config({})
 
